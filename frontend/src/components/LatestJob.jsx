8636f19cf0fe6085d5105da7247c1b635088020d
Ayan Manna <ayanmanna858@gmail.com>
2025-07-30 19:17:46 +0530

    new

--- frontend/src/components/LatestJob.jsx
+++ frontend/src/components/LatestJob.jsx
@@ -4,7 +4,9 @@ import { useSelector } from "react-redux";
 import { store } from "@/Redux/store";
 const randomjob = [1, 2, 3, 4, 5, 6, 7, 8];
 const LatestJob = () => {
-  const {alljobs} = useSelector(store=> store.job)
+  const jobState = useSelector((store) => store.job);
+  const alljobs = jobState?.alljobs || [];
+
   return (
     <div className="max-w-7xl mx-auto my-20">
       <h1 className="text-4xl font-bold ">
@@ -12,9 +14,13 @@ const LatestJob = () => {
         <span className="text-[#6A38C2]">Job Openings</span>
       </h1>
       <div className="grid grid-cols-3 gap-4 my-5 ">
-        { alljobs.length <= 0 ?  <span>no Jobs found </span> : alljobs.slice(0,6).map((job) => (
-          <LatestJobCard key={ job._id} job={job}/>
-        ))}
+        {alljobs.length === 0 ? (
+          <span>No jobs found</span>
+        ) : (
+          alljobs
+            .slice(0, 6)
+            .map((job) => <LatestJobCard key={job._id} job={job} />)
+        )}
       </div>
     </div>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "./components/ui/sonner";
-import { store } from './Redux/store'
-import { Provider } from 'react-redux'
+import { store } from "./Redux/store";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
+const persistor = persistStore(store);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loding={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
     </Provider>
-    <Toaster />
+    
   </StrictMode>
 );
