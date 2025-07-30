5866255cdade252b113b3501c793952bc130b0bc
Ayan Manna <ayanmanna858@gmail.com>
2025-07-31 01:09:56 +0530

    new

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import { singleUplode } from "../middlewares/multer.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import {
   getcompanis,
@@ -11,6 +11,6 @@ const companyroute = express.Router();
 companyroute.route("/regestercompany").post(isAuthenticated, regestercompany);
 companyroute.route("/getcompanis").get(isAuthenticated, getcompanis);
 companyroute.route("/get/:id").get(isAuthenticated, getCompanyId);
-companyroute.route("/update/:id").put(isAuthenticated, updateCompany);
+companyroute.route("/update/:id").put(isAuthenticated, singleUplode ,updateCompany);
 
 export default companyroute;
