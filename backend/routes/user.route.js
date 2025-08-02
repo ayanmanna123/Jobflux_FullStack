49d8f09dccdadc1bc7f1ef7db6c1163af89d9b5f
Ayan Manna <ayanmanna858@gmail.com>
2025-08-02 17:00:48 +0530

    updated

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,7 +4,6 @@ import {
   logout,
   register,
   updateProfile,
-  updateProfilePhoto,
 } from "../controllers/user.controllers.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { singleUplode } from "../middlewares/multer.js";
@@ -17,5 +16,5 @@ router.route("/logout").get(logout);
 
 // ✅ Fix applied here:
 router.route("/profile/updateProfile").post(isAuthenticated, singleUplode, updateProfile);
-router.route("/upload-profile-photo").put ( isAuthenticated, singleUplode, updateProfilePhoto);
+
 export default router;
--- frontend/src/components/LatestJobCard.jsx
+++ frontend/src/components/LatestJobCard.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { Badge } from "@/components/ui/badge";
 import { useNavigate } from "react-router-dom";
-import { Button } from "./ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-
 const LatestJobCard = ({ job }) => {
   const naviget = useNavigate();
   return (
@@ -11,21 +8,10 @@ const LatestJobCard = ({ job }) => {
       className="p-5 rounded-md shadow-xl bg-white border-gray-100 cursor-pointer "
       onClick={() => naviget(`/discription/${job?._id}`)}
     >
-      <div className="flex items-center gap-3">
-        <div>
-          <Button className="p-6" variant="outline" size="icon">
-            <Avatar className="w-10 h-10">
-              <AvatarImage src={job?.company?.logo} alt="Logo" />
-              <AvatarFallback>CL</AvatarFallback>
-            </Avatar>
-          </Button>
-        </div>
-        <div>
-          <h1 className="font-medium text-lg ">{job?.company?.name}</h1>
-          <p className="text-sm text-gray-500">India</p>
-        </div>
+      <div>
+        <h1 className="font-medium text-lg ">{job?.company?.name}</h1>
+        <p className="text-sm text-gray-500">India</p>
       </div>
-
       <div>
         <h1 className="font-bold text-lg my-2 ">{job?.title}</h1>
         <p className="text-sm text-gray-600 ">{job?.description}</p>
