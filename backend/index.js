2b344225e8a55668a6d9b3e37bc63263e78d5a67
Ayan Manna <ayanmanna858@gmail.com>
2025-08-03 11:25:05 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 3000;
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://jobflux-full-stack.vercel.app"
+    "https://jobflux-full-stack-8sja.vercel.app"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
